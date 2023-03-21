237c2814811fd75038ca6b73b39bb6d90eacdaea
Philippe Bernard <philippe.bernard@gmail.com>
2023-03-21 15:11:09 +0100

    Do need to create the docker group

--- scripts/setup_hellobuddy_supserset.sh
+++ scripts/setup_hellobuddy_supserset.sh
@@ -18,7 +18,6 @@ sudo docker run hello-world
 sudo systemctl enable docker
 
 # From https://docs.docker.com/engine/install/linux-postinstall/
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 docker run hello-world
